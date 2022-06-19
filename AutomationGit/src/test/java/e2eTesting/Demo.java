740a02428f2f8a218d99408c9caa70d77ae35465
Rahul <rprahulpandey23@gmail.com>
2022-06-19 23:10:43 +0530

    committed from original repo

--- AutomationGit/src/test/java/e2eTesting/Demo.java
+++ AutomationGit/src/test/java/e2eTesting/Demo.java
@@ -10,6 +10,18 @@ public class Demo {
 		System.out.println("dummy");
 		System.out.println("demo");
 		System.out.println("data");
+		System.out.println("rahul");
+		System.out.println("kumar");
+		System.out.println("pandey");
+		System.out.println("varun");
+		System.out.println("kumar");
 	}
 
+	@Test
+	public void demoTest() {
+
+		System.out.println("dummy");
+		System.out.println("demo");
+		System.out.println("data");
+	}
 }
